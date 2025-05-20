a29015f73ac574ac5fe671a4b07c7f350445ef59
Ramon Tovar <rtovar.develops@gmail.com>
2025-05-21 08:45:18 +1000

    feat: add player on click

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Player from "../components/player.svelte";
+
+    let numPlayers: number[] = $state([]);
+    const AddPlayer = () => {
+        numPlayers.push(numPlayers.length);
+    }
+</script>
+
+<button onclick={AddPlayer}>Add player</button>
+{#each numPlayers as player}
+    <Player />
+{/each}
