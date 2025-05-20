da9b11db80648c39fb9dfe301147e7bc9867e3f8
Ramon Tovar <rtovar.develops@gmail.com>
2025-05-21 07:43:20 +1000

    feat: enzo added player component

---
+++ src/components/player.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let name: string = "John";
+    let dutchPile: number = 0;
+    let blitzPile: number = 0;
+    let score: number = 0;
+</script>
+
+<style>
+    #player-container {
+        display: flex;
+    }
+</style>
+
+<div id="player-container">
+    <h1>{name}</h1>
+    <div>
+        <label for="dutch-pile">Dutch pile</label>
+        <input type="number" id="dutch-pile" name="dutch-pile" min=0 value=0 />
+    </div>
+    <div>
+        <label for="blitz-pile">Blitz pile</label>
+        <input type="number" id="blitz-pile" name="blitz-pile" min=0 value=0 />
+    </div>
+    <p>Score: {score}</p>
+</div>
